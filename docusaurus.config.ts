728839bb5c7e0ba5a1beecc304985f77bf562afa
frost <Bear-Frost@users.noreply.github.com>
2024-11-03 03:58:40 +0800

    chore: add a site URL for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,17 +7,11 @@ const config: Config = {
   tagline: "Add Stylish Borders with Ease",
   favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "https://bordex.netlify.app",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
